41313c98fdf84457a4e3919d8d4cffd4eb1e2346
Steven Ly <1stevenly92@gmail.com>
2020-03-30 15:55:40 -0700

    got everything to work

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+
+
+
+    var numbers = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    var suites = ["Clubs", "Hearts", "Diamond", "Spade"];
+    var list = [];
+
+
+    $("#form").submit(function (event) {
+        event.preventDefault();
+        $("#form").hide();
+
+        suites.forEach(function (suite) {
+            var list = numbers.map(function (number) {
+                return (number + " of " + suite + "</br>");
+
+
+
+                
+            })
+
+
+
+            $("#result").append(list);
+
+            console.log(list)
+
+
+
+
+            
+
+
+        })
+
+
+
+
+    })
+
+
+});
